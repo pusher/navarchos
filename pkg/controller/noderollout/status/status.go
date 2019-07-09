0c2b8b275db01360b0bed4f85edae2b1177c170a
Joel Speed <Joel.speed@hotmail.co.uk>
2019-07-09 16:42:28 +0100

    Add basic structure to NodeRollout controller

---
+++ pkg/controller/noderollout/status/status.go
@@ -0,0 +1,15 @@
+package status
+
+import (
+	"fmt"
+
+	navarchosv1alpha1 "github.com/pusher/navarchos/pkg/apis/navarchos/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// UpdateStatus merges the status in the existing instance with the information
+// provided in the handler.Result and then updates the instance if there is any
+// difference between the new and updated status
+func UpdateStatus(c client.Client, instance *navarchosv1alpha1.NodeRollout, result *Result) error {
+	return fmt.Errorf("Method not implemented")
+}
---
+++ pkg/controller/noderollout/status/types.go
@@ -0,0 +1,6 @@
+package status
+
+// Result is used as the basis to updating the status of the NodeRollout.
+// It contains information gathered during a single run of the reconcile loop.
+type Result struct {
+}
