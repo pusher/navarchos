cc38fa102f2887bf9dab0cdc6542da3e1c4b63a6
Joel Speed <Joel.speed@hotmail.co.uk>
2019-07-10 11:49:47 +0100

    Add test suite for NodeRollout status

---
+++ pkg/controller/noderollout/status/status_test.go
@@ -0,0 +1,314 @@
+package status
+
+import (
+	"errors"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	navarchosv1alpha1 "github.com/pusher/navarchos/pkg/apis/navarchos/v1alpha1"
+	"github.com/pusher/navarchos/test/utils"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var _ = Describe("NodeRollout Status Suite", func() {
+	var c client.Client
+	var m utils.Matcher
+
+	var nodeRollout *navarchosv1alpha1.NodeRollout
+	var result *Result
+
+	const timeout = time.Second * 5
+	const consistentlyTimeout = time.Second
+
+	BeforeEach(func() {
+		var err error
+		c, err = client.New(cfg, client.Options{})
+		Expect(err).NotTo(HaveOccurred())
+		m = utils.Matcher{Client: c}
+
+		nodeRollout = utils.ExampleNodeRollout.DeepCopy()
+		m.Create(nodeRollout).Should(Succeed())
+
+		result = &Result{}
+	})
+
+	AfterEach(func() {
+		utils.DeleteAll(cfg, timeout,
+			&navarchosv1alpha1.NodeRolloutList{},
+		)
+	})
+
+	Context("UpdateStatus", func() {
+		var updateErr error
+
+		JustBeforeEach(func() {
+			updateErr = UpdateStatus(c, nodeRollout, result)
+		})
+
+		Context("when the phase is set in the Result", func() {
+			var phase navarchosv1alpha1.NodeRolloutPhase
+
+			BeforeEach(func() {
+				phase = navarchosv1alpha1.RolloutPhaseInProgress
+				Expect(nodeRollout.Status.Phase).ToNot(Equal(phase))
+				result.Phase = &phase
+			})
+
+			PIt("updates the phase in the status", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("Phase", Equal(phase)))
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("when no existing ReplacementsCreated is set", func() {
+			var replacementsCreated []string
+
+			BeforeEach(func() {
+				replacementsCreated = []string{"example-master-1", "example-master-2", "example-worker-1", "example-worker-2"}
+				Expect(nodeRollout.Status.ReplacementsCreated).To(BeEmpty())
+				result.ReplacementsCreated = replacementsCreated
+			})
+
+			PIt("sets the ReplacementsCreated field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsCreated", Equal(replacementsCreated)))
+			})
+
+			PIt("sets the ReplacementsCreatedCount field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsCreatedCount", Equal(len(replacementsCreated))))
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("when an existing ReplacementsCreated is set", func() {
+			var replacementsCreated []string
+			var existingReplacementsCreated []string
+
+			BeforeEach(func() {
+				// Set up the existing expected state
+				existingReplacementsCreated = []string{"example-master-1", "example-worker-1"}
+				m.Update(nodeRollout, func(obj utils.Object) utils.Object {
+					nr, _ := obj.(*navarchosv1alpha1.NodeRollout)
+					nr.Status.ReplacementsCreated = existingReplacementsCreated
+					nr.Status.ReplacementsCreatedCount = len(existingReplacementsCreated)
+					return nr
+				}, timeout).Should(Succeed())
+
+				replacementsCreated = []string{"example-master-1", "example-master-2", "example-worker-1", "example-worker-2"}
+				result.ReplacementsCreated = replacementsCreated
+			})
+
+			It("does not update the ReplacementsCreated field", func() {
+				m.Consistently(nodeRollout, consistentlyTimeout).Should(utils.WithNodeRolloutStatusField("ReplacementsCreated", Equal(existingReplacementsCreated)))
+			})
+
+			It("does not update the ReplacementsCreatedCount field", func() {
+				m.Consistently(nodeRollout, consistentlyTimeout).Should(utils.WithNodeRolloutStatusField("ReplacementsCreatedCount", Equal(len(existingReplacementsCreated))))
+			})
+
+			PIt("returns an error", func() {
+				Expect(updateErr.Error()).To(Equal("cannot update ReplacementsCreated, field is immutable once set"))
+			})
+		})
+
+		Context("when no existing ReplacementsCompleted is set", func() {
+			var replacementsCompleted []string
+
+			BeforeEach(func() {
+				replacementsCompleted = []string{"example-master-1", "example-master-2", "example-worker-1", "example-worker-2"}
+				Expect(nodeRollout.Status.ReplacementsCompleted).To(BeEmpty())
+				result.ReplacementsCompleted = replacementsCompleted
+			})
+
+			PIt("sets the ReplacementsCompleted field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsCompleted", Equal(replacementsCompleted)))
+			})
+
+			PIt("sets the ReplacementsCompletedCount field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsCompletedCount", Equal(len(replacementsCompleted))))
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("when an existing ReplacementsCompleted is set", func() {
+			var replacementsCompleted []string
+			var existingReplacementsCompleted []string
+			var expectedReplacementsCompleted []string
+
+			BeforeEach(func() {
+				// Set up the existing expected state
+				existingReplacementsCompleted = []string{"example-master-1", "example-worker-1"}
+				m.Update(nodeRollout, func(obj utils.Object) utils.Object {
+					nr, _ := obj.(*navarchosv1alpha1.NodeRollout)
+					nr.Status.ReplacementsCompleted = existingReplacementsCompleted
+					nr.Status.ReplacementsCompletedCount = len(existingReplacementsCompleted)
+					return nr
+				}, timeout).Should(Succeed())
+
+				replacementsCompleted = []string{"example-master-2", "example-worker-2"}
+				result.ReplacementsCompleted = replacementsCompleted
+
+				expectedReplacementsCompleted = append(replacementsCompleted, existingReplacementsCompleted...)
+			})
+
+			PIt("joins the new and existing ReplacementsCompleted field", func() {
+				m.Eventually(nodeRollout, timeout).Should(
+					utils.WithNodeRolloutStatusField("ReplacementsCompleted", ConsistOf(expectedReplacementsCompleted)),
+				)
+			})
+
+			PIt("updates the ReplacementsCompletedCount field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsCompletedCount", Equal(len(expectedReplacementsCompleted))))
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("when no existing ReplacementsFailed is set", func() {
+			var replacementsFailed []string
+
+			BeforeEach(func() {
+				replacementsFailed = []string{"example-master-1", "example-master-2", "example-worker-1", "example-worker-2"}
+				Expect(nodeRollout.Status.ReplacementsFailed).To(BeEmpty())
+				result.ReplacementsFailed = replacementsFailed
+			})
+
+			PIt("sets the ReplacementsFailed field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsFailed", Equal(replacementsFailed)))
+			})
+
+			PIt("sets the ReplacementsFailedCount field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsFailedCount", Equal(len(replacementsFailed))))
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("when an existing ReplacementsFailed is set", func() {
+			var replacementsFailed []string
+			var existingReplacementsFailed []string
+
+			BeforeEach(func() {
+				// Set up the existing expected state
+				existingReplacementsFailed = []string{"example-master-1", "example-worker-1"}
+				m.Update(nodeRollout, func(obj utils.Object) utils.Object {
+					nr, _ := obj.(*navarchosv1alpha1.NodeRollout)
+					nr.Status.ReplacementsFailed = existingReplacementsFailed
+					nr.Status.ReplacementsFailedCount = len(existingReplacementsFailed)
+					return nr
+				}, timeout).Should(Succeed())
+
+				replacementsFailed = []string{"example-master-2", "example-worker-2"}
+				result.ReplacementsFailed = replacementsFailed
+			})
+
+			PIt("updates the ReplacementsFailed field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsFailed", Equal(replacementsFailed)))
+			})
+
+			PIt("updates the ReplacementsFailedCount field", func() {
+				m.Eventually(nodeRollout, timeout).Should(utils.WithNodeRolloutStatusField("ReplacementsFailedCount", Equal(len(replacementsFailed))))
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("when the ReplacementsCompletedError is not set in the Result", func() {
+			PIt("updates the status condition", func() {
+				m.Eventually(nodeRollout, timeout).Should(
+					utils.WithNodeRolloutStatusField("Conditions",
+						ContainElement(SatisfyAll(
+							utils.WithNodeRolloutConditionField("Type", Equal(navarchosv1alpha1.ReplacementsCreatedType)),
+							utils.WithNodeRolloutConditionField("Status", Equal(corev1.ConditionTrue)),
+							utils.WithNodeRolloutConditionField("Reason", BeEmpty()),
+							utils.WithNodeRolloutConditionField("Message", BeEmpty()),
+						)),
+					),
+				)
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("when the ReplacementsCompletedError is set in the Result", func() {
+			BeforeEach(func() {
+				result.ReplacementsCompletedError = errors.New("error creating replacements")
+				result.ReplacementsCompletedReason = "CompletedErrorReason"
+			})
+
+			PIt("updates the status condition", func() {
+				m.Eventually(nodeRollout, timeout).Should(
+					utils.WithNodeRolloutStatusField("Conditions",
+						ContainElement(SatisfyAll(
+							utils.WithNodeRolloutConditionField("Type", Equal(navarchosv1alpha1.ReplacementsCreatedType)),
+							utils.WithNodeRolloutConditionField("Status", Equal(corev1.ConditionFalse)),
+							utils.WithNodeRolloutConditionField("Reason", Equal(result.ReplacementsCompletedReason)),
+							utils.WithNodeRolloutConditionField("Message", Equal(result.ReplacementsCompletedError.Error())),
+						)),
+					),
+				)
+			})
+
+			PIt("does not cause an error", func() {
+				Expect(updateErr).To(BeNil())
+			})
+		})
+
+		Context("ReplacementsCompletedError and ReplacementsCompletedReason must be set together", func() {
+			Context("if only ReplacementsCompleteError is set", func() {
+				BeforeEach(func() {
+					result.ReplacementsCompletedError = errors.New("error")
+				})
+
+				PIt("causes an error", func() {
+					Expect(updateErr).ToNot(BeNil())
+				})
+			})
+
+			Context("if only ReplacementsCompleteReason is set", func() {
+				BeforeEach(func() {
+					result.ReplacementsCompletedReason = "test"
+				})
+
+				PIt("causes an error", func() {
+					Expect(updateErr).ToNot(BeNil())
+				})
+			})
+
+			Context("if both are set", func() {
+				BeforeEach(func() {
+					result.ReplacementsCompletedError = errors.New("error")
+					result.ReplacementsCompletedReason = "test"
+				})
+
+				PIt("does not cause an error", func() {
+					Expect(updateErr).To(BeNil())
+				})
+			})
+
+			Context("if neither are set", func() {
+				PIt("does not cause an error", func() {
+					Expect(updateErr).To(BeNil())
+				})
+			})
+		})
+	})
+})
---
+++ test/utils/matchers.go
@@ -0,0 +1,170 @@
+/*
+Copyright 2019 Pusher Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/onsi/gomega"
+	gtypes "github.com/onsi/gomega/types"
+	navarchosv1alpha1 "github.com/pusher/navarchos/pkg/apis/navarchos/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Matcher has Gomega Matchers that use the controller-runtime client
+type Matcher struct {
+	Client client.Client
+}
+
+// Object is the combination of two interfaces as a helper for passing
+// Kubernetes objects between methods
+type Object interface {
+	runtime.Object
+	metav1.Object
+}
+
+// UpdateFunc modifies the object fetched from the API server before sending
+// the update
+type UpdateFunc func(Object) Object
+
+// Create creates the object on the API server
+func (m *Matcher) Create(obj Object, extras ...interface{}) gomega.GomegaAssertion {
+	err := m.Client.Create(context.TODO(), obj)
+	return gomega.Expect(err, extras)
+}
+
+// Delete deletes the object from the API server
+func (m *Matcher) Delete(obj Object, extras ...interface{}) gomega.GomegaAssertion {
+	err := m.Client.Delete(context.TODO(), obj)
+	return gomega.Expect(err, extras)
+}
+
+// Update udpates the object on the API server by fetching the object
+// and applying a mutating UpdateFunc before sending the update
+func (m *Matcher) Update(obj Object, fn UpdateFunc, intervals ...interface{}) gomega.GomegaAsyncAssertion {
+	key := types.NamespacedName{
+		Name:      obj.GetName(),
+		Namespace: obj.GetNamespace(),
+	}
+	update := func() error {
+		err := m.Client.Get(context.TODO(), key, obj)
+		if err != nil {
+			return err
+		}
+		return m.Client.Update(context.TODO(), fn(obj))
+	}
+	return gomega.Eventually(update, intervals...)
+}
+
+// Get gets the object from the API server
+func (m *Matcher) Get(obj Object, intervals ...interface{}) gomega.GomegaAsyncAssertion {
+	key := types.NamespacedName{
+		Name:      obj.GetName(),
+		Namespace: obj.GetNamespace(),
+	}
+	get := func() error {
+		return m.Client.Get(context.TODO(), key, obj)
+	}
+	return gomega.Eventually(get, intervals...)
+}
+
+// Consistently continually gets the object from the API for comparison
+func (m *Matcher) Consistently(obj Object, intervals ...interface{}) gomega.GomegaAsyncAssertion {
+	return m.consistentlyObject(obj, intervals...)
+}
+
+// consistentlyObject gets an individual object from the API server
+func (m *Matcher) consistentlyObject(obj Object, intervals ...interface{}) gomega.GomegaAsyncAssertion {
+	key := types.NamespacedName{
+		Name:      obj.GetName(),
+		Namespace: obj.GetNamespace(),
+	}
+	get := func() Object {
+		err := m.Client.Get(context.TODO(), key, obj)
+		if err != nil {
+			panic(err)
+		}
+		return obj
+	}
+	return gomega.Consistently(get, intervals...)
+}
+
+// Eventually continually gets the object from the API for comparison
+func (m *Matcher) Eventually(obj runtime.Object, intervals ...interface{}) gomega.GomegaAsyncAssertion {
+	// If the object is a list, return a list
+	if meta.IsListType(obj) {
+		return m.eventuallyList(obj, intervals...)
+	}
+	if o, ok := obj.(Object); ok {
+		return m.eventuallyObject(o, intervals...)
+	}
+	//Should not get here
+	panic("Unknown object.")
+}
+
+// eventuallyObject gets an individual object from the API server
+func (m *Matcher) eventuallyObject(obj Object, intervals ...interface{}) gomega.GomegaAsyncAssertion {
+	key := types.NamespacedName{
+		Name:      obj.GetName(),
+		Namespace: obj.GetNamespace(),
+	}
+	get := func() Object {
+		err := m.Client.Get(context.TODO(), key, obj)
+		if err != nil {
+			panic(err)
+		}
+		return obj
+	}
+	return gomega.Eventually(get, intervals...)
+}
+
+// eventuallyList gets a list type  from the API server
+func (m *Matcher) eventuallyList(obj runtime.Object, intervals ...interface{}) gomega.GomegaAsyncAssertion {
+	list := func() runtime.Object {
+		err := m.Client.List(context.TODO(), &client.ListOptions{}, obj)
+		if err != nil {
+			panic(err)
+		}
+		return obj
+	}
+	return gomega.Eventually(list, intervals...)
+}
+
+// WithNodeRolloutStatusField gets the value of the named field from the
+// NodeRollouts Status
+func WithNodeRolloutStatusField(field string, matcher gtypes.GomegaMatcher) gtypes.GomegaMatcher {
+	return gomega.WithTransform(func(obj *navarchosv1alpha1.NodeRollout) interface{} {
+		r := reflect.ValueOf(obj.Status)
+		f := reflect.Indirect(r).FieldByName(field)
+		return f.Interface()
+	}, matcher)
+}
+
+// WithNodeRolloutConditionField gets the value of the named field from the
+// NodeRolloutCondition
+func WithNodeRolloutConditionField(field string, matcher gtypes.GomegaMatcher) gtypes.GomegaMatcher {
+	return gomega.WithTransform(func(obj navarchosv1alpha1.NodeRolloutCondition) interface{} {
+		r := reflect.ValueOf(obj)
+		f := reflect.Indirect(r).FieldByName(field)
+		return f.Interface()
+	}, matcher)
+}
