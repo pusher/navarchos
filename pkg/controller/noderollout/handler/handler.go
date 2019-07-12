6f389e1087fbb5adeafa3e0d83ed4f45e4bde8e4
Joel Speed <Joel.speed@hotmail.co.uk>
2019-07-12 15:40:14 +0100

    Add Options to set up of NodeRollout Handler

--- pkg/controller/noderollout/handler/handler.go
+++ pkg/controller/noderollout/handler/handler.go
@@ -8,18 +8,34 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// This is the maximum age that Completed/Failed NodeRollouts are allowed to
-// be before they are cleaned up by the Handler.
-const maxNodeRolloutAge = time.Hour * 48
+// Options are used to configure the NodeRolloutHandler
+type Options struct {
+	// MaxAge determines the maximum age a NodeRollout should be before it is
+	// garbage collected
+	MaxAge *time.Duration
+}
+
+// Complete defaults any values that are not explicitly set
+func (o *Options) Complete() {
+	if o.MaxAge == nil {
+		maxAge := 48 * time.Hour
+		o.MaxAge = &maxAge
+	}
+}
 
 // NodeRolloutHandler handles the business logic within the NodeRollout controller.
 type NodeRolloutHandler struct {
 	client client.Client
+	maxAge time.Duration
 }
 
 // NewNodeRolloutHandler creates a new NodeRolloutHandler
-func NewNodeRolloutHandler(c client.Client) *NodeRolloutHandler {
-	return &NodeRolloutHandler{client: c}
+func NewNodeRolloutHandler(c client.Client, opts *Options) *NodeRolloutHandler {
+	opts.Complete()
+	return &NodeRolloutHandler{
+		client: c,
+		maxAge: *opts.MaxAge,
+	}
 }
 
 // Handle performs the business logic of the NodeRollout and returns information
