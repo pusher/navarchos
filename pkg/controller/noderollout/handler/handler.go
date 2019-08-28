97f9ee3e79504a6f05ddb72c6c29ee290eab07a1
Theo Barber-Bany <theo.barberbany@pusher.com>
2019-08-28 11:41:30 +0100

    Add Handle() structure

--- pkg/controller/noderollout/handler/handler.go
+++ pkg/controller/noderollout/handler/handler.go
@@ -41,5 +41,25 @@ func NewNodeRolloutHandler(c client.Client, opts *Options) *NodeRolloutHandler {
 // Handle performs the business logic of the NodeRollout and returns information
 // in a Result
 func (h *NodeRolloutHandler) Handle(instance *navarchosv1alpha1.NodeRollout) *status.Result {
+	switch instance.Status.Phase {
+	case navarchosv1alpha1.RolloutPhaseNew:
+		return h.handleNew(instance)
+	case navarchosv1alpha1.RolloutPhaseInProgress:
+		return h.handleInProgress(instance)
+	case navarchosv1alpha1.RolloutPhaseCompleted:
+		return h.handleCompleted(instance)
+	default:
+		return &status.Result{}
+	}
+
+}
+
+func (h *NodeRolloutHandler) handleNew(instance *navarchosv1alpha1.NodeRollout) *status.Result {
+	return &status.Result{}
+}
+func (h *NodeRolloutHandler) handleInProgress(instance *navarchosv1alpha1.NodeRollout) *status.Result {
+	return &status.Result{}
+}
+func (h *NodeRolloutHandler) handleCompleted(instance *navarchosv1alpha1.NodeRollout) *status.Result {
 	return &status.Result{}
 }
