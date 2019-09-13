2fd4352fe49ab50ba5d8330a23129a1ce30ddd39
Joel Speed <Joel.speed@hotmail.co.uk>
2019-09-13 14:14:23 +0100

    Allow NodeReplacement handler to return errors

--- pkg/controller/nodereplacement/nodereplacement_controller.go
+++ pkg/controller/nodereplacement/nodereplacement_controller.go
@@ -95,7 +95,10 @@ func (r *ReconcileNodeReplacement) Reconcile(request reconcile.Request) (reconci
 		return reconcile.Result{}, err
 	}
 
-	result := r.handler.Handle(instance)
+	result, err := r.handler.Handle(instance)
+	if err != nil {
+		return reconcile.Result{}, fmt.Errorf("error handling replacement %s: %v", instance.GetName(), err)
+	}
 	err = status.UpdateStatus(r.Client, instance, result)
 	if err != nil {
 		return reconcile.Result{}, fmt.Errorf("error updating status: %v", err)
