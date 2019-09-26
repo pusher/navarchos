11db899afe9f7083b0d81e6ce8c82f20c36f7b9c
Theo Barber-Bany <theo.barberbany@pusher.com>
2019-09-26 14:24:35 +0100

    Replacement handler updates status between phases

--- pkg/controller/nodereplacement/handler/handler.go
+++ pkg/controller/nodereplacement/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"time"
 
 	navarchosv1alpha1 "github.com/pusher/navarchos/pkg/apis/navarchos/v1alpha1"
@@ -47,8 +48,19 @@ func (h *NodeReplacementHandler) Handle(instance *navarchosv1alpha1.NodeReplacem
 
 	switch instance.Status.Phase {
 	default:
-		instance.Status.Phase = navarchosv1alpha1.ReplacementPhaseNew
-		fallthrough // this is important, we want one instance to be handled to completion without a requeue if possible
+		newPhase := navarchosv1alpha1.ReplacementPhaseNew
+		// Update status before starting next phase
+		err = status.UpdateStatus(h.client, instance, &status.Result{
+			Phase: &newPhase,
+		})
+		if err != nil {
+			// This is an API error which means other errors are also likely,
+			// bail and requeue We will reattempt the status update anyway where
+			// this is called
+			return result, fmt.Errorf("error updating status: %v", err)
+		}
+
+		fallthrough // This is important, we want one instance to be handled to completion without a requeue if possible
 	case navarchosv1alpha1.ReplacementPhaseNew:
 		result, err = h.handleNew(instance)
 		if err != nil {
@@ -57,14 +69,24 @@ func (h *NodeReplacementHandler) Handle(instance *navarchosv1alpha1.NodeReplacem
 		if result.Requeue {
 			return result, nil
 		}
-		fallthrough // this is important, we want one instance to be handled to completion without a requeue if possible
+
+		// Update status before starting next phase
+		err = status.UpdateStatus(h.client, instance, result)
+		if err != nil {
+			// This is an API error which means other error are also likely,
+			// bail and requeue We will reattempt the status update anyway where
+			// this is called
+			return result, fmt.Errorf("error updating status: %v", err)
+		}
+
+		fallthrough // This is important, we want one instance to be handled to completion without a requeue if possible
 	case navarchosv1alpha1.ReplacementPhaseInProgress:
 		result, err = h.handleInProgress(instance, result)
 		if err != nil {
 			return result, err
 		}
 	}
-
+	// Nothing left to do
 	return result, nil
 }
 
